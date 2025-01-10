9e934cd209d68979ee165f4be45aebb278975502
이지혜 <59763645+dev-jihye@users.noreply.github.com>
2025-01-10 21:31:42 +0900

    ✨ 토글 스위치 추가하기

---
+++ src/caseStudy/ToggleSwitch.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+const ToggleSwitch = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setIsOn(!isOn);
+        }}
+      >
+        {isOn ? 'On' : 'Off'}
+      </button>
+    </div>
+  );
+};
+
+export default ToggleSwitch;
